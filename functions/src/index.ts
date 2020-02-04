1f788483b70449b0cd61374948b904c57dde7cf3
INTERNETL0RD <xtebanchallenger@gmail.com>
2020-02-04 14:47:40 +0100

    functions ready and deployed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+export const createUserDoc = functions.region("europe-west1").auth.user().onCreate((user) =>
+    db.collection("users").add({ email: user.email, photoUrl: user.photoURL, uid: user.uid, phoneNumber: user.phoneNumber})
+);
