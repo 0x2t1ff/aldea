1df6a9164614789794f218f4f6b934ed4a19d52d
unknown <javier.ucler@dinobyte.es>
2020-09-10 13:17:05 +0200

    user navigation in vouch screen

--- lib/ui/widgets/vouch_item.dart
+++ lib/ui/widgets/vouch_item.dart
@@ -6,8 +6,9 @@ import '../shared/app_colors.dart' as custcolor;
 
 class VouchItem extends StatefulWidget {
   final User vouch;
+  final Function goToProfile;
   final int index;
-  const VouchItem({Key key, this.vouch, this.index}) : super(key: key);
+  const VouchItem({Key key, this.vouch, this.index, this.goToProfile}) : super(key: key);
 
   @override
   _VouchItemState createState() => _VouchItemState();
@@ -19,39 +20,44 @@ class _VouchItemState extends State<VouchItem> {
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      Container(
-        width: devicesize.screenWidth(context),
-        height: devicesize.screenHeight(context) * 0.103,
-        color: widget.index % 2 == 0
-            ? custcolor.backgroundColor
-            : custcolor.backgroundColor,
-        child: Row(children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: devicesize.screenWidth(context) * 0.045),
+     GestureDetector(onTap: (){
+       widget.goToProfile();
+     },
             child: Container(
-              decoration: quickstrikePicDecoration,
-              child: CircleAvatar(
-                radius: devicesize.screenWidth(context) * 0.065,
-                backgroundImage: NetworkImage(widget.vouch.picUrl),
+          width: devicesize.screenWidth(context),
+          height: devicesize.screenHeight(context) * 0.103,
+          color: widget.index % 2 == 0
+              ? custcolor.backgroundColor
+              : custcolor.darkGrey,
+          child: Row(children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: devicesize.screenWidth(context) * 0.045),
+              child:Container(
+                  decoration: quickstrikePicDecoration,
+                  child: CircleAvatar(
+                    radius: devicesize.screenWidth(context) * 0.065,
+                    backgroundImage: NetworkImage(widget.vouch.picUrl),
+                  ),
+                ),
+              ),
+           
+            Padding(
+              padding:
+                  EdgeInsets.only(left: devicesize.screenWidth(context) * 0.045),
+              child: Text(
+                widget.vouch.name,
+                style: TextStyle(
+                  color: custcolor.almostWhite,
+                  fontSize: 20,
+                  fontFamily: 'Raleway',
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding:
-                EdgeInsets.only(left: devicesize.screenWidth(context) * 0.045),
-            child: Text(
-              widget.vouch.name,
-              style: TextStyle(
-                color: custcolor.almostWhite,
-                fontSize: 20,
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-        ]),
-      ),
+          ]),
+        ),
+     ),
     ]);
   }
 }
